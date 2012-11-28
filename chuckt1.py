776f0ba7b5a75bdbde6c9449e73b9656278b9457
ericdanz <ericdanziger@gmail.com>
2012-11-27 23:31:10 -0500

    change chuck1

--- chuckt1.py
+++ chuckt1.py
@@ -1,5 +1,5 @@
 
-#I'm going to want the forest, a fitter, and whatever this is averaged
+#I'm going to want the forest, a fitter (logistic regression? elastic net?), and whatever this is averaged
 
 import csv as csv
 import numpy as np
