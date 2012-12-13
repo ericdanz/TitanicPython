065be81ebad36b0212e479fac488dd249bffc0d0
ericdanz <ericdanziger@gmail.com>
2012-12-13 01:13:57 -0500

    updating gradient boosting classifier

--- gradientboostingclassifier.py
+++ gradientboostingclassifier.py
@@ -9,119 +9,8 @@ from sklearn.cross_validation import StratifiedKFold
 from sklearn.grid_search import GridSearchCV
 from collections import Counter
 from pandas import *
+import efunctions as ef
 
-
-def cabin_letter(x):
-	return{
-		'A':1,
-		'B':2,
-		'C':3,
-		'D':4,
-		'E':5,
-		'F':6,
-		'G':7,
-		}.get(x,0)
-
-def num (s):
-	try:
-		return int(s)
-	except ValueError:
-		return float(s)
-		
-def fixdataSVM (data):
-#split the cabin column to find the last split (the last if there are multiple cabins)
-#then assign a number based on the cabin letter
-	cabin_letter_list = []
-	for i in data[0::,9]:
-		splitter = i.rsplit(' ',1)[-1]
-		if splitter:
-			splitter = cabin_letter(splitter[0])
-		else:
-			splitter = 0
-		cabin_letter_list.append(splitter)
-
-	data[0::,9] = cabin_letter_list
-	
-	#print data[0,9]
-
-	#Male = 1, female = 0:
-	data[data[0::,3]=='male',3] = 1
-	data[data[0::,3]=='female',3] = 0
-	#embark c=0, s=1, q=2
-	data[data[0::,10] =='C',10] = 0
-	data[data[0::,10] =='S',10] = 1
-	data[data[0::,10] =='Q',10] = 2
-	data[data[0::,10] == '',10] = 3
-	#find the most common embark point, put it in the blanks
-	data[data[0::,10] == 3,10] = max(Counter(data[data[0::,10] != 3,10]))
-	#take means for age for each class
-
-	age = []
-	firstclassfare = []
-	secondclassfare = []
-	thirdclassfare = []
-	
-	for x in data:
-		if x[4]:
-			age.append(np.float(x[4]))
-		if x[1] == '1' and x[8]:
-			firstclassfare.append(np.float(x[8]))
-		if x[1] == '2' and x[8]:
-			secondclassfare.append(np.float(x[8]))
-		if x[1] == '3' and x[8]:
-			thirdclassfare.append(np.float(x[8]))
-
-	ageaverage = int(np.median(age))
-	
-	firstclassfareaverage = int(np.mean(firstclassfare))
-	secondclassfareaverage = int(np.mean(secondclassfare))
-	thirdclassfareaverage = int(np.mean(thirdclassfare))
-	
-	#put those averages back into the '' ages
-
-	for i in xrange(np.size(data[0::,0])):
-		try:
-			float(data[i,4])
-		except ValueError:
-			data[i,4] = ageaverage
-		try:
-			float(data[i,8])
-		except ValueError:
-			if data[i,1] == '1':
-				data[i,8] = firstclassfareaverage
-			if data[i,1] == '2':
-				data[i,8] = secondclassfareaverage
-			if data[i,1] == '3':
-				data[i,8] = thirdclassfareaverage
-				
-	#clean up the name and ticket elements
-	data = np.delete(data,[2,7,9],1)
-		
-	#change strings to float
-	for i in xrange(np.size(data[0::,0])):
-		for y in range(8):
-			try:
-				data[i,y] = num(data[i,y])
-			except ValueError:
-				print y 
-				print data[i,y]
-				print '--'
-				#data[i,y] = num(0)
-	
-	return data
-
-def scaleData (trainer,tester):
-	deparray = trainer[0::,0]
-	df = DataFrame(trainer.astype(np.float))
-	df_norm = (df-df.mean())/(df.max()-df.min())
-	trainer = np.array(df_norm)
-	trainer[0::,0] = deparray
-	dfT = DataFrame(tester.astype(np.float))
-	dfT_scaled = (dfT-df.mean())/(df.max()-df.min())
-	tester = np.array(dfT_scaled)
-	tester[0::,0] = 0
-	bothArray = [trainer,tester]
-	return bothArray
 	
 #import training data
 csv_file_object = csv.reader(open('train.csv', 'rb')) #Load in the csv file
@@ -141,11 +30,11 @@ test_data = np.insert(test_data,[0], 0, axis=1)
 
 #normalize data frame, remove ticket and name, fix cabin to be just the letter
 #in the future, scale the data by the train set and apply that to test set
-train_data = fixdataSVM(train_data)
-test_data = fixdataSVM(test_data)
-#train_test = scaleData(train_data, test_data)
-#train_data = train_test[0]
-#test_data = train_test[1]
+train_data = ef.fixdataSVM(train_data)
+test_data = ef.fixdataSVM(test_data)
+train_test = ef.scaleData(train_data, test_data)
+train_data = train_test[0]
+test_data = train_test[1]
 #print train_data[2]
 #print test_data[2]
 
@@ -164,15 +53,54 @@ test_data = fixdataSVM(test_data)
 #    open_file_object.writerow(row)
 #    i += 1
 
-
 #GBC
-gb = GradientBoostingClassifier(learn_rate = 0.01, n_estimators = 500)
+nEst = 1001
+lR = .3
+
+gb = GradientBoostingClassifier(learn_rate = lR, n_estimators = nEst)
 gb.fit(train_data[0::,1::],train_data[0::,0])
 gbcResults = gb.predict(test_data[0::,1::])
 
 
+#Cross-Validate the RF
+cvScore = []
+savedGBC = []
+cv = StratifiedKFold(train_data[0::,0], 15)
+for train,test in cv:
+	cvGBC = GradientBoostingClassifier(learn_rate = lR, n_estimators = nEst).fit(train_data[train,1::].astype(np.float),train_data[train,0].astype(np.float))
+	savedGBC.append(cvGBC)
+	thisOutput = cvGBC.predict(train_data[test,1::].astype(np.float))
+	cvScore.append(ef.compare(thisOutput.astype(np.float),train_data[test,0].astype(np.float)))
+
+print "CV Scores:"
+for score in cvScore:
+	print score
+
+print "Against the whole training set accuracy:"
+sfOutput = []
+for s in savedGBC:
+	thisOutput = s.predict(train_data[0::,1::].astype(np.float))
+	print ef.compare(thisOutput.astype(np.float),train_data[0::,0].astype(np.float))
+	sfOutput.append(thisOutput)
+	
+averageOutput = []
+sfOutput = np.array(sfOutput)
+averageOutput =  np.mean(sfOutput[0::].astype(np.int), axis=0)
+averageOutput = averageOutput.astype(np.int)
+
+print "Averaged accuracy:"
+print ef.compare(averageOutput.astype(np.float),train_data[0::,0].astype(np.float))
+
+print "single GBC accuracy:"
+print ef.compare (gb.predict(train_data[0::,1::]).astype(np.float),train_data[0::,0].astype(np.float))
+
+
+
+
+
 
 #record the GBC results 
+'''
 open_file_object = csv.writer(open("gbc.csv", "wb"))
 test_file_object = csv.reader(open('test.csv', 'rb')) #Load in the csv file
 test_file_object.next()
@@ -182,7 +110,7 @@ for row in test_file_object:
 	row.insert(0,gbcResults[i].astype(np.uint8))
 	open_file_object.writerow(row)
 	i += 1
-
+'''
 
 
 
