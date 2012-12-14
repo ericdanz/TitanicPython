eb01c2bc2617dbfdd1a3be799217bcefd05fd64b
ericdanz <ericdanziger@gmail.com>
2012-12-14 00:27:52 -0500

    add two new averages

--- averageswithchangingthreshold.py
+++ averageswithchangingthreshold.py
@@ -9,7 +9,7 @@ csv_file_objectg = csv.reader(open('genderclassmodel2.csv', 'rb')) #Load in the 
 csv_file_objectf = csv.reader(open('myfirstforest2.csv', 'rb')) #Load in the csv file
 csv_file_objectr = csv.reader(open('rbf.csv', 'rb')) #Load in the csv file
 csv_file_objectn = csv.reader(open('normalizedforest.csv', 'rb')) #Load in the csv file
-csv_file_objectgb = csv.reader(open('sexbasedgbc.csv', 'rb')) #Load in the csv file
+csv_file_objectgb = csv.reader(open('loosersexbasedgbc.csv', 'rb')) #Load in the csv file
 
 
 
@@ -45,8 +45,8 @@ gbdata = np.array(gbdata)
 fdata = np.array(fdata)
 rdata = np.array(rdata)
 avgsurvived = []
-tempavg = ['1','2','3','4','5']
-#tempavg = ['1','2','3']
+#tempavg = ['1','2','3','4','5']
+tempavg = ['1','2','3']
 print xrange(np.size(fdata[0::,0]))
 print xrange(np.size(gdata[0::,0]))
 
@@ -54,19 +54,19 @@ print xrange(np.size(gdata[0::,0]))
 for i in xrange(np.size(gdata[0::,0])):
 	tempavg[0] = gdata[i,0].astype(float)
 	tempavg[1] = fdata[i,0].astype(float)
-	tempavg[2] = rdata[i,0].astype(float)
-	tempavg[3] = gbdata[i,0].astype(float)
-	tempavg[4] = ndata[i,0].astype(float)
+	#tempavg[2] = rdata[i,0].astype(float)
+	tempavg[2] = gbdata[i,0].astype(float)
+	#tempavg[4] = ndata[i,0].astype(float)
 	#print tempavg
 	print np.mean(tempavg)
-	if np.mean(tempavg) > .6:
+	if np.mean(tempavg) > .5:
 		avgdata.append(1)
 	else:
 		avgdata.append(0)
 	#avgdata.append((np.mean(tempavg)))
 	
 
-open_file_object = csv.writer(open("avgforest6threshold.csv", "wb"))
+open_file_object = csv.writer(open("avgfirsttwoandlsgbc.csv", "wb"))
 test_file_object = csv.reader(open('test.csv', 'rb'))
 
 test_file_object.next()
